ebab3166a6e92ceaf114c9da14db5455fb806693
leblanc-i <idrissaamadouousmane@gmail.com>
2024-03-05 15:34:00 +0100

    Projet Terminé

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 class Question {
-  constructor (text, choices, answer) {
+  constructor(text, choices, answer) {
     this.text = text;
     this.choices = choices;
     this.answer = answer;
   }
 
-  // Fonction qui va nous permettre de savoir si l'utilisateur à bien repondu
+  // Methode qui va nous permettre de savoir si l'utilisateur à bien repondu
   isCorrect(choice) {
     return choice === this.answer;
   }
@@ -34,10 +34,136 @@ const questions = [
   ),
 ];
 
+// La classe Quiz prendra en compte toutes les questions du tableau
 class Quiz {
-  constructor (questions) {
+  constructor(questions) {
     this.score = 0;
     this.questions = questions;
+    this.currentQuestionIndex = 0; // Pour connaitre la position de la question
+  }
+  // Methode qui va nous retourner la question sur laquelle on se trouve
+  getCurrentQuestion() {
+    return this.questions[this.currentQuestionIndex];
+  }
+  // Methode pour le choix de l'utilisateur
+  guess(answer) {
+    if (this.getCurrentQuestion().isCorrect(answer)) {
+      this.score++;
+    }
+    this.currentQuestionIndex++;
+  }
+  // Methode pour savoir si cest finis(et bien on sait que c'est lorsque currentQuestionIndex est egal la longueur du tableau)
+  hasEnded() {
+    return this.currentQuestionIndex >= this.questions.length;
+  }
+
+  reset() {
+    const parent = document.querySelector(".container");
+    parent.innerHTML = `
+
+      <div id="quiz">
+        <h1><span>Q</span>uiz <i class="far fa-question-circle"></i></h1>
+
+        <h2 id="question"></h2>
+
+        <h3 id="score"></h3>
+
+        <h3 id="reprendre"></h3>
+
+        <div class="choices">
+          <button id="guess0" class="btn">
+            <p id="choice0"></p>
+          </button>
+
+          <button id="guess1" class="btn">
+            <p id="choice1"></p>
+          </button>
+
+          <button id="guess2" class="btn">
+            <p id="choice2"></p>
+          </button>
+
+          <button id="guess3" class="btn">
+            <p id="choice3"></p>
+          </button>
+        </div>
+
+        <p id="progress"></p>
+
+      </div>
+    `
+    this.score = 0;
     this.currentQuestionIndex = 0;
+
   }
 }
+
+// On cree un objet qui va gerer l'affichage du Quiz
+const display = {
+  // function qui prend en paramettre l'id et le text de la question
+  elementShown: function (id, text) {
+    let element = document.getElementById(id);
+    element.innerHTML = text;
+  },
+  question: function () {
+    this.elementShown("question", quiz.getCurrentQuestion().text);
+  },
+  choices: function () {
+    let choices = quiz.getCurrentQuestion().choices;
+
+    // Fonction pour gerer le choix de l'utilisateur
+    guessHandler = (id, guess) => {
+      // on ajoute un evenement qui sera adosser par tout nos quatre bouton
+      document.getElementById(id).onclick = function () {
+        quiz.guess(guess);
+        quizApp(); // On testera la reponse
+      };
+    };
+    // Affichage choix + prise en compte du choix
+    for (let i = 0; i < choices.length; i++) {
+      this.elementShown("choice" + i, choices[i]);
+      guessHandler("guess" + i, choices[i]);
+    }
+  },
+
+  progress: function () {
+    this.elementShown(
+      "progress",
+      `question ${quiz.currentQuestionIndex + 1} sur ${quiz.questions.length} `
+    );
+  },
+
+  endQuiz: function () {
+    let endQuizHtml = `
+      <h1>Quiz terminé</h1>
+      <h3>Votre score est de : ${quiz.score} / ${quiz.questions.length} </h3>
+      <button id="again">Recommencer</button>
+    `;
+    this.elementShown("quiz", endQuizHtml);
+    let againBtn = document.getElementById("again");
+    againBtn.addEventListener("click", () => {
+      quiz.reset();
+      quizApp();
+    })
+  },
+};
+
+// On cree la logique du Quiz
+
+const quizApp = () => {
+  // Si le quiz est terminer tu fais ça
+  if (quiz.hasEnded()) {
+    // Ecran de fin
+    display.endQuiz();
+    // Sinon tu fais ça
+  } else {
+    // Afficher les questions les choix et la proression
+    display.question();
+    display.choices();
+    display.progress();
+  }
+};
+
+// Et enfin on cree le Quiz
+let quiz = new Quiz(questions);
+quizApp();
